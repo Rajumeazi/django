e54cab85112f9d2503e26bd18a7fcb14d259322c
Md. Rakibul Hasan Raju Meazi <rajumeazi@gmail.com>
2022-09-21 16:15:50 +0600

    settings.py and urls.ph
    settings.py and urls.phy has been updated

--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -1,7 +1,21 @@
 
+from http.client import HTTPResponse
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+
+def home(request):
+    return HttpResponse('Home Page')
+
+
+def welcome(requestgit):
+    return HttpResponse('Welcome Page')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('welcome/',welcome),
 ]
